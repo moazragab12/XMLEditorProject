c4adba3298362b23e1787311a7b9df020f4018bc
MoustafaHashem <moustafahashem7770@gmail.com>
2024-11-24 15:48:59 +0200

    edits to new features

--- src/main/java/com/xml/editor/Functions.java
+++ src/main/java/com/xml/editor/Functions.java
@@ -1,5 +1,7 @@
 package com.xml.editor;
 
+import javafx.scene.image.ImageView;
+
 public interface Functions {
     static  String[] check(String[] s){
         return s;
@@ -26,4 +28,21 @@ public interface Functions {
     static  String[] decomp(String[] s){
         return s;
     }
+    static ImageView draw(String[] s){
+        return new ImageView();
+    }
+    static String[] networkAnalysis(String[] s){
+        return  s;
+    }
+    static String[] suggest(String[] s,int id){
+        return  s;
+    }
+    static String[] wordSearch(String[] s,String word){
+        return  s;
+    }
+    static String[] topicSearch(String[] s,String topic){
+        return  s;
+    }
+
+
 }
