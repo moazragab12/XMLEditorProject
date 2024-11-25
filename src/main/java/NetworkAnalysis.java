f5f88328e249067ad51ecfff7a3c87e82d81019b
Mayar0311 <mayarwalid22@gmail.com>
2024-11-25 21:06:24 +0200

    ...

---
+++ src/main/java/NetworkAnalysis.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class NetworkAnalysis {
+    User [] users;
+
+    public User[] getUsers(){
+        return users;
+    }
+
+    //get info from xml file and save users in User array
+    public void ReadFile(String s){
+
+    }
+
+    public User mostInfluencer(){
+        int index = 0;
+        int mostfollowed = users[0].followers.size();
+        for(int i = 0; i < users.length; i++){
+            if(mostfollowed < users[i].followers.size()){
+                mostfollowed = users[i].followers.size();
+                index = i;
+            }
+        }
+
+        return users[index];
+    }
+
+    public User mostActive(){
+        int index = 0;
+
+
+        return users[index];
+    }
+
+    public ArrayList<User> mutualFollowers(String U1, String U2){
+        ArrayList<User> mutualF = new ArrayList<>();
+
+        return mutualF;
+    }
+
+    public ArrayList<User> suggestedFollowers(){
+        ArrayList<User> suggest = new ArrayList<>();
+
+
+
+        return suggest;
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class User {
+    String ID;
+    String name;
+    ArrayList<String> followers = new ArrayList<String>();
+    ArrayList<String> posts = new ArrayList<String>();
+
+    public void print(){
+        System.out.println("User ID is: " + ID);
+        System.out.println("User name is: " + name);
+        //System.out.println("Number of followers: " + followers.size());
+        System.out.println("User's followers: " + followers);
+    }
+
+    @Override
+    public String toString(){
+        return ("user: " + this.name + " - Id: " + this.ID);
+    }
+}
