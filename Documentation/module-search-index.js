df74b040e0876726bd11e77a9f1500abb789b8a7
Moaz Ragab <moazr8365@gmail.com>
2024-12-30 13:47:51 +0200

    generate java doc

--- Documentation/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"com.xml.editor"}];updateSearchResults();
--- Documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.xml.editor","l":"com.xml.editor"}];updateSearchResults();
